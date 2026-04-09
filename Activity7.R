f1b294c3da4b2aa279000a4344d8ce871022e13b
jaehmz <james.maisano11@gmail.com>
2026-04-09 15:37:21 +0000

    worked on in class questions 1 and 2

--- Activity7.R
+++ Activity7.R
@@ -1,1 +1,28 @@
 # James Maisano
+
+# Installing packages
+install.packages(c("dplyr","ggplot2","olsrr","PerformanceAnalytics"))
+library(dplyr)
+library(ggplot2)
+library(olsrr)
+library(PerformanceAnalytics)
+
+# Reading in activity 7 data
+ghg <- read.csv("/cloud/project/activity07/Deemer_GHG_Data.csv")
+
+# log transform methane fluxes
+ghg$log.ch4 <- log(ghg$ch4+1)
+# log transform age
+ghg$log.age <- log(ghg$age)
+# log transform DIP
+ghg$log.DIP <- log(ghg$DIP+1)
+# log transform percipitation
+ghg$log.precip <- log(ghg$precipitation)
+
+# printing out unique regions
+unique(ghg$Region)
+
+# binary variable for boreal region
+ghg$BorealV <- ifelse(ghg$Region == "Boreal",1,0)
+# binary variable for tropical region
+ghg$TropicalV <- ifelse(ghg$Region == "Tropical",1,0)
