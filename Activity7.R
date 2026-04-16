c940c1b4071a174835c85205a38369db7d920f5b
jaehmz <james.maisano11@gmail.com>
2026-04-16 15:38:06 +0000

    finished both tutorials, begin q1 on hw

--- Activity7.R
+++ Activity7.R
@@ -1,12 +1,15 @@
 # James Maisano
 
 # Installing packages
-install.packages(c("dplyr","ggplot2","olsrr","PerformanceAnalytics"))
+install.packages(c("dplyr","ggplot2","olsrr","PerformanceAnalytics","lubridate","forecast"))
 library(dplyr)
 library(ggplot2)
 library(olsrr)
 library(PerformanceAnalytics)
+library(lubridate)
+library(forecast)
 
+# In Class Activity Ch 6 ----
 # Reading in activity 7 data
 ghg <- read.csv("/cloud/project/activity07/Deemer_GHG_Data.csv")
 
@@ -26,3 +29,190 @@ unique(ghg$Region)
 ghg$BorealV <- ifelse(ghg$Region == "Boreal",1,0)
 # binary variable for tropical region
 ghg$TropicalV <- ifelse(ghg$Region == "Tropical",1,0)
+# binary variable for alpine region
+ghg$AlpineV <- ifelse(ghg$Alpine == "yes",1,0)
+# binary variable for known hydropower
+ghg$HydroV <- ifelse(ghg$hydropower == "yes",1,0)
+
+# multiple regression
+# creates a model object
+mod.full <- lm(log.ch4 ~ airTemp+
+                 log.age+mean.depth+
+                 log.DIP+
+                 log.precip+ BorealV, data=ghg) #uses the data argument to specify dataframe
+
+res.full <- rstandard(mod.full)
+fit.full <- fitted.values(mod.full)
+
+# qq plot
+qqnorm(res.full, pch=19, col="grey50")
+qqline(res.full)
+
+# shapiro-wilks test
+shapiro.test(res.full)
+
+plot(fit.full,res.full, pch=19, col="grey50")
+abline(h=0)
+
+# isolate continuous model variables into data frame:
+
+reg.data <- data.frame(ghg$airTemp,
+                       ghg$log.age,ghg$mean.depth,
+                       ghg$log.DIP,
+                       ghg$log.precip)
+
+# make a correlation matrix 
+chart.Correlation(reg.data, histogram=TRUE, pch=19)
+
+# run stepwise
+full.step <- ols_step_forward_aic(mod.full)
+# view table
+full.step 
+
+# check full model
+full.step$model
+# plot AIC over time
+plot(full.step )
+
+# prediction with interval for predicting a point
+predict.lm(mod.full, data.frame(airTemp=20,log.age=log(2),
+                                mean.depth=15,log.DIP=3,
+                                log.precip=6, BorealV=0),
+           interval="prediction")
+
+# look at prediction with 95% confidence interval of the mean
+
+predict.lm(mod.full, data.frame(airTemp=20,log.age=log(2),
+                                mean.depth=15,log.DIP=3,
+                                log.precip=6, BorealV=0),
+           interval="confidence")
+
+# Activity 7 Questions ----
+# Question 1
+# See PDF
+# Question 2
+# See PDF
+# Question 3
+# Improvement on Regression
+mod.improve <- lm(log.ch4 ~ airTemp+surface.area+Residence.Time..days.+
+                 log.age+mean.depth+
+                 log.DIP+
+                 log.precip+ BorealV, data=ghg)
+# Analyze regression
+summary(mod.improve)
+
+# Checking Assumptions
+res.improve <- rstandard(mod.improve)
+fit.improve <- fitted.values(mod.improve)
+# Normality
+# qq plot
+qqnorm(res.improve, pch=19, col="grey50")
+qqline(res.improve)
+
+# shapiro-wilks test
+shapiro.test(res.improve)
+
+# Checking Residuals 
+plot(fit.improve,res.improve, pch=19, col="grey50")
+abline(h=0)
+
+# Checking multicollinearity
+# isolate continuous model variables into data frame:
+
+reg.data.improve <- data.frame(ghg$airTemp,
+                       ghg$log.age,ghg$mean.depth,
+                       ghg$log.DIP,
+                       ghg$log.precip,
+                       ghg$surface.area,
+                       ghg$Residence.Time..days.)
+
+# make a correlation matrix 
+chart.Correlation(reg.data.improve, histogram=TRUE, pch=19)
+
+# Question 4
+
+# In class Activity Ch 7 ----
+# Reading in the data
+ETdat <- read.csv("/cloud/project/activity07/ETdata.csv")
+
+unique(ETdat$crop)
+
+# average fields for each month for almonds
+almond <- ETdat %>% # ET data
+  filter(crop == "Almonds") %>% # only use almond fields
+  group_by(date) %>% # calculate over each date
+  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE)) # average fields
+
+# visualize the data
+ggplot(almond, aes(x=ymd(date),y=ET.in))+
+  geom_point()+
+  geom_line()+
+  labs(x="year", y="Monthy evapotranspiration (in)")
+
+# almond ET time series
+almond_ts <- ts(almond$ET.in, # data
+                start = c(2016,1), #start year 2016, month 1
+                #first number is unit of time and second is observations within a unit
+                frequency= 12) # frequency of observations in a unit
+
+# decompose almond ET time series
+almond_dec <- decompose(almond_ts)
+# plot decomposition
+plot(almond_dec)
+
+almondTrend <- almond_dec$trend
+almondSeason <- almond_dec$seasonal
+
+acf(na.omit(almond_ts), # remove missing data
+    lag.max = 24) # look at 2 years (24 months)
+
+pacf.plot <- pacf(na.omit(almond_ts))
+
+almond_y <- na.omit(almond_ts)
+model1 <- arima(almond_y , # data 
+                order = c(1,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model1
+
+model4 <- arima(almond_y , # data 
+                order = c(4,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model4
+# calculate fit
+AR_fit1 <- almond_y - residuals(model1) 
+AR_fit4 <- almond_y - residuals(model4)
+#plot data
+plot(almond_y)
+# plot fit
+points(AR_fit1, type = "l", col = "tomato3", lty = 2, lwd=2)
+points(AR_fit4, type = "l", col = "darkgoldenrod4", lty = 2, lwd=2)
+legend("topleft", c("data","AR1","AR4"),
+       lty=c(1,2,2), lwd=c(1,2,2), 
+       col=c("black", "tomato3","darkgoldenrod4"),
+       bty="n")
+
+newAlmond <- forecast(model4)
+newAlmond
+
+#make dataframe for plotting
+newAlmondF <- data.frame(newAlmond)
+
+# set up dates
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newAlmondF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# make a plot with data and predictions including a prediction interval
+ggplot() +
+  geom_line(data = almond, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(almond$date[1]),newAlmondF$dateF[24])+  # Plotting original data
+  geom_line(data = newAlmondF, aes(x = dateF, y = Point.Forecast),
+            col="red") +  # Plotting model forecasts
+  geom_ribbon(data=newAlmondF, 
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+ # uncertainty interval
+  theme_classic()+
+  labs(x="year", y="Evapotranspiration (in)")
+
+# Homework Questions
+# Question 1
+# Transforming co2 data
+co2.trans <- 1/(co2+1000)
