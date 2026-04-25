2d08a663c788750405130b19d271e95db934079c
jaehmz <james.maisano11@gmail.com>
2026-04-25 18:05:48 +0000

    finished q 3 and worked on 4

--- Activity7.R
+++ Activity7.R
@@ -336,3 +336,136 @@ ggplot(pist, aes(x=ymd(date),y=ET.in))+
 pist_ts <- ts(pist$ET.in,
               start = c(2016,1),
               frequency= 12)
+# decompose pistachio ET time series
+pist_dec <- decompose(pist_ts)
+# plot decomposition
+plot(pist_dec)
+
+# plot PACF to help identify AR order
+pacf.plot <- pacf(na.omit(pist_ts))
+
+# remove missing data
+pist_y <- na.omit(pist_ts)
+
+# fit AR(1) model
+model1 <- arima(pist_y,
+                order = c(1,0,0))
+model1
+
+# fit AR(4) model
+model4 <- arima(pist_y,
+                order = c(4,0,0))
+model4
+
+# calculate fit for both models
+AR_fit1 <- pist_y - residuals(model1)
+AR_fit4 <- pist_y - residuals(model4)
+# plot data and model fits to compare AR(1) and AR(4)
+plot(pist_y)
+points(AR_fit1, type = "l", col = "darkgreen", lty = 2, lwd=2)
+points(AR_fit4, type = "l", col = "lightgreen", lty = 2, lwd=2)
+legend("topleft", c("data","AR1","AR4"),
+       lty=c(1,2,2), lwd=c(1,2,2),
+       col=c("darkgreen", "green4","lightgreen"),
+       bty="n")
+
+# forecast future ET using AR(4) model
+newPist <- forecast(model4)
+newPist
+# make dataframe for plotting
+newPistF <- data.frame(newPist)
+
+# set up dates for forecast period
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newPistF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# plot historical data and forecast with 95% prediction interval
+ggplot() +
+  geom_line(data = pist, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(pist$date[1]),newPistF$dateF[24])+
+  geom_line(data = newPistF, aes(x = dateF, y = Point.Forecast),
+            col="green4") +
+  geom_ribbon(data=newPistF,
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+
+  theme_classic()+
+  labs(x="year", y="Evapotranspiration (in)")
+
+# Fallow Cropland
+
+# filter data for fallow/idle cropland and average fields for each month
+fallow <- ETdat %>%
+  filter(crop == "Fallow/Idle Cropland") %>%
+  group_by(date) %>%
+  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE))
+
+# visualize the data
+ggplot(fallow, aes(x=ymd(date),y=ET.in))+
+  geom_point()+
+  geom_line()+
+  labs(x="year", y="Monthy evapotranspiration (in)")
+
+# fallow ET time series
+fallow_ts <- ts(fallow$ET.in,
+                start = c(2016,1),
+                frequency= 12)
+
+# decompose fallow ET time series
+fallow_dec <- decompose(fallow_ts)
+# plot decomposition
+plot(fallow_dec)
+
+# plot ACF to examine autocorrelation structure
+acf(na.omit(fallow_ts),
+    lag.max = 24)
+
+# plot PACF to help identify AR order
+pacf.plot <- pacf(na.omit(fallow_ts))
+
+# remove missing data
+fallow_y <- na.omit(fallow_ts)
+# fit AR(1) model
+model1 <- arima(fallow_y,
+                order = c(1,0,0))
+model1
+
+# fit AR(4) model
+model4 <- arima(fallow_y,
+                order = c(4,0,0))
+model4
+
+# calculate fit for both models
+AR_fit1 <- fallow_y - residuals(model1)
+AR_fit4 <- fallow_y - residuals(model4)
+# plot data and model fits to compare AR(1) and AR(4)
+plot(fallow_y)
+points(AR_fit1, type = "l", col = "grey", lty = 2, lwd=2)
+points(AR_fit4, type = "l", col = "darkgrey", lty = 2, lwd=2)
+legend("topleft", c("data","AR1","AR4"),
+       lty=c(1,2,2), lwd=c(1,2,2),
+       col=c("grey", "lightgrey","darkgrey"),
+       bty="n")
+
+# forecast future ET using AR(4) model
+newFallow <- forecast(model4)
+newFallow
+
+# make dataframe for plotting
+newFallowF <- data.frame(newFallow)
+# set up dates for forecast period
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newFallowF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# plot historical data and forecast with 95% prediction interval
+ggplot() +
+  geom_line(data = fallow, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(fallow$date[1]),newFallowF$dateF[24])+
+  geom_line(data = newFallowF, aes(x = dateF, y = Point.Forecast),
+            col="grey") +
+  geom_ribbon(data=newFallowF,
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+
+  theme_classic()+
+  labs(x="year", y="Evapotranspiration (in)")
